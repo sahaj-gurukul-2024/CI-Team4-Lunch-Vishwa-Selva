05c73e24db9f04c063c9aa00c8bd0d9af9a4d6f0
selvakannanjr <4mselva@gmail.com>
2024-02-26 20:30:47 +0530

    Update build.gradle.kts
    add docker task

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,9 @@ java {
     sourceCompatibility = JavaVersion.toVersion("21")
 }
 
+tasks.named<MicronautDockerfile>("dockerfile") {
+    baseImage.set("eclipse-temurin:21.0.2_13-jdk")
+}
 
 graalvmNative.toolchainDetection.set(false)
 micronaut {
